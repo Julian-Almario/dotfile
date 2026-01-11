8a57916e04f93792ba6eb8c0732e253f27e01114
Julian Almario <doctorz.0525@gmail.com>
2026-01-10 22:54:40 -0500

    add code ranger_devicon

--- installmeprograms.sh
+++ installmeprograms.sh
@@ -11,6 +11,9 @@ sudo pacman -S starship lsd fastfetch gtop
 
 sudo pacman -S noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-roboto ttf-roboto-mono otf-latinmodern-math - ttf-liberation ttf-gentium-plus adobe-source-code-pro-fonts ttf-ubuntu-font-family ttf-mononoki-nerd
 
+# ranger_devicons
+# git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+
 
 # Comand code progresion
 
